abcbe714ac037f07f521b84ee43ff63f04821da4
Tom Bredemeier <tbredemeier@barrington220.org>
2014-05-22 12:57:05 -0500

    Working multiplayer game

--- ViewController.m
+++ ViewController.m
@@ -16,11 +16,18 @@
 @property (strong, nonatomic) IBOutlet DieLabel *dieLabel4;
 @property (strong, nonatomic) IBOutlet DieLabel *dieLabel5;
 @property (strong, nonatomic) IBOutlet DieLabel *dieLabel6;
-@property (strong, nonatomic) IBOutlet UILabel *userScoreLabel;
+@property (strong, nonatomic) IBOutlet UILabel *redScoreLabel;
+@property (strong, nonatomic) IBOutlet UILabel *blueScoreLabel;
+@property (strong, nonatomic) IBOutlet UILabel *redTurnLabel;
+@property (strong, nonatomic) IBOutlet UILabel *blueTurnLabel;
 @property NSMutableArray *selectedDice;
 @property NSMutableArray *scoredDice;
-@property NSInteger score;
+@property NSInteger redScore;
+@property NSInteger blueScore;
+@property NSInteger turnScore;
 @property NSInteger numDiesScoring;
+@property BOOL isRedTurn;
+@property BOOL turnOver;
 
 @end
 
@@ -43,15 +50,18 @@
 - (IBAction)onRollButtonPressed:(id)sender
 {
     [self scoreSelectedDice];
-    // fast enumerate through all the controlls in the view
-    for(DieLabel *object in self.view.subviews)
+    if(!self.turnOver)
     {
-        // check to see if the control is a DieLabel
-        if([object isKindOfClass:[DieLabel class]])
+        // fast enumerate through all the controlls in the view
+        for(DieLabel *object in self.view.subviews)
         {
-            if(![self.scoredDice containsObject:object])
+            // check to see if the control is a DieLabel
+            if([object isKindOfClass:[DieLabel class]])
             {
-                [object roll];
+                if(![self.scoredDice containsObject:object])
+                {
+                    [object roll];
+                }
             }
         }
     }
@@ -60,7 +70,7 @@
 - (IBAction)onDoneButtonPressed:(id)sender
 {
     [self scoreSelectedDice];
-    [self resetGame];
+    [self endTurn];
 }
 
 - (IBAction)onQuitButtonPressed:(id)sender
@@ -76,15 +86,40 @@
 
 - (void)resetGame
 {
-    self.score = 0;
-    self.numDiesScoring = 0;
-    [self.scoredDice removeAllObjects];
+    self.redScore = 0;
+    self.blueScore = 0;
+    self.isRedTurn = NO;
+    [self endTurn];
+}
+
+- (void)endTurn
+{
+    self.turnScore = 0;
+    self.isRedTurn = !self.isRedTurn;
+    if(self.isRedTurn)
+    {
+        [self.redTurnLabel setBackgroundColor:[UIColor lightGrayColor]];
+        [self.blueTurnLabel setBackgroundColor:[UIColor whiteColor]];
+    }
+    else
+    {
+        [self.blueTurnLabel setBackgroundColor:[UIColor lightGrayColor]];
+        [self.redTurnLabel setBackgroundColor:[UIColor whiteColor]];
+    }
+    [self resetDice];
+    self.turnOver = NO;
+}
+
+- (void)resetDice
+{
     for(DieLabel *object in self.view.subviews)
     {
         // check to see if the control is a DieLabel
         if([object isKindOfClass:[DieLabel class]])
             [object reset];
     }
+    [self.scoredDice removeAllObjects];
+    self.numDiesScoring = 0;
 }
 
 - (void)scoreSelectedDice
@@ -95,7 +130,7 @@
     NSInteger num4s = 0;
     NSInteger num5s = 0;
     NSInteger num6s = 0;
-    NSInteger turnScore = 0;
+    NSInteger rollScore = 0;
 
     // get pip totals
     for(DieLabel *dieLabel in self.selectedDice)
@@ -130,13 +165,13 @@
     {
         if (num1s >= 3)
         {
-            turnScore += 1000;  // three 1's are 1000 points
+            rollScore += 1000;  // three 1's are 1000 points
             num1s -= 3;
             self.numDiesScoring += 3;
         }
         else
         {
-            turnScore += 100;   // each additional 1 is 100 points
+            rollScore += 100;   // each additional 1 is 100 points
             num1s --;
             self.numDiesScoring ++;
         }
@@ -144,21 +179,21 @@
 
     while(num2s >= 3)
     {
-        turnScore += 200;       // three 2's are 200 points
+        rollScore += 200;       // three 2's are 200 points
         num2s -= 3;
         self.numDiesScoring += 3;
     }
 
     while(num3s >= 3)
     {
-        turnScore += 300;       // three 3's are 300 points
+        rollScore += 300;       // three 3's are 300 points
         num3s -= 3;
         self.numDiesScoring += 3;
     }
 
     while(num4s >= 3)
     {
-        turnScore += 400;       // three 4's are 400 points
+        rollScore += 400;       // three 4's are 400 points
         num4s -= 3;
         self.numDiesScoring += 3;
     }
@@ -167,13 +202,13 @@
     {
         if (num5s >= 3)
         {
-            turnScore += 500;   // three 5's are 500 points
+            rollScore += 500;   // three 5's are 500 points
             num5s -= 3;
             self.numDiesScoring += 3;
         }
         else
         {
-            turnScore += 50;    // each additional 5 is worth 50 points
+            rollScore += 50;    // each additional 5 is worth 50 points
             num5s --;
             self.numDiesScoring ++;
         }
@@ -181,17 +216,40 @@
 
     while(num6s >= 3)
     {
-        turnScore += 600;       // three 6's are 600 points
+        rollScore += 600;       // three 6's are 600 points
         num6s -= 3;
         self.numDiesScoring ++;
     }
 
-    if(turnScore > 0)
-        self.score += turnScore;
+    self.turnScore += rollScore;
+    if(self.isRedTurn)
+    {
+        if(rollScore > 0)
+            self.redScore += rollScore;
+        else
+        {
+            if(self.redScore >= self.turnScore && self.turnScore > 0)
+            {
+                self.redScore -= self.turnScore;
+                self.turnOver = YES;
+            }
+        }
+        [self.redScoreLabel setText:[NSString stringWithFormat:@"%d", self.redScore]];
+    }
     else
-        self.score = 0;
-
-    self.userScoreLabel.text = [NSString stringWithFormat:@"%d", self.score];
+    {
+        if(rollScore > 0)
+            self.blueScore += rollScore;
+        else
+        {
+            if(self.blueScore >= self.turnScore && self.turnScore > 0)
+            {
+                self.blueScore -= self.turnScore;
+                self.turnOver = YES;
+            }
+        }
+        [self.blueScoreLabel setText:[NSString stringWithFormat:@"%d", self.blueScore]];
+    }
 
     // add selected dice to scored dice array
     for(DieLabel *dieLabel in self.selectedDice)
@@ -204,17 +262,7 @@
 
     // check for "hot dice"
     if(self.numDiesScoring == 6)
-    {
-        [self.scoredDice removeAllObjects];
-        for(DieLabel *object in self.view.subviews)
-        {
-            // check to see if the control is a DieLabel
-            if([object isKindOfClass:[DieLabel class]])
-            {
-                [object reset];
-            }
-        }
-    }
+        [self resetDice];
 }
 
 @end
